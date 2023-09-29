f9eb296ed48c4136798ed7e27a96c366d64bb76a
Daniel Brendel <dbrendel1988@gmail.com>
2023-09-29 22:23:25 +0200

    List plants of room

--- app/views/plants.php
+++ app/views/plants.php
@@ -5,15 +5,19 @@
 		<div class="column-overlay">
 			<h1>{{ __('app.plants') }}</h1>
 
-			<div>
+			<div class="margin-vertical">
                 <a class="button is-success" href="javascript:void(0);" onclick="document.getElementById('inpLocationId').value = {{ $location }}; window.vue.bShowAddPlant = true;">{{ __('app.add_plant') }}</a>
             </div>
 
 			<div class="plants">
 				@foreach ($plants as $plant)
-                    <div class="plant-card" style="background-image: url('{{ $plant->get('photo') }}');">
-						<div class="plant-card-title">{{ $plant->get('name') }}</div>
-					</div>
+					<a href="{{ url('/plants/details/' . $plant->get('id')) }}">
+						<div class="plant-card" style="background-image: url('{{ asset('img/' . $plant->get('photo')) }}');">
+							<div class="plant-card-overlay">
+								<div class="plant-card-title">{{ $plant->get('name') }}</div>
+							</div>
+						</div>
+					</a>
 				@endforeach
 			</div>
 		</div>
