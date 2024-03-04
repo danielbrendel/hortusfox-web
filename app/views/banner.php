99b90d2f2ff0eecc811972996a9cbfd9795b5b7e
Daniel Brendel <dbrendel1988@gmail.com>
2024-03-04 15:12:02 +0100

    #120 Optional banner icon

--- app/views/banner.php
+++ app/views/banner.php
@@ -1,1 +1,7 @@
-<div class="banner" style="background-image: url('{{ asset('img/banner.jpg') }}');"></div>
+<div class="banner" style="background-image: url('{{ asset('img/banner.jpg') }}');">
+    @if (file_exists(public_path() . '/img/banner-icon.png'))
+        <div class="banner-icon">
+            <img src="{{ asset('img/banner-icon.png') }}" alt="banner-icon"/>
+        </div>
+    @endif
+</div>
