333de130a5438a4afc70fa0c0d736ee9b69db461
Daniel Brendel <dbrendel1988@gmail.com>
2024-05-05 15:23:52 +0200

    Better displaying of hint

--- app/views/history.php
+++ app/views/history.php
@@ -68,6 +68,12 @@
 			</div>
 		@endforeach
 	@else
-		<span class="plants-empty">{{ __('app.content_empty') }}</span>
+		<div class="plants-empty">
+			<div class="plants-empty-image">
+				<img src="{{ asset('img/plantsempty.png') }}" alt="image"/>
+			</div>
+
+			<div class="plants-empty-text">{{ __('app.content_empty') }}</div>
+		</div>
 	@endif
 </div>
