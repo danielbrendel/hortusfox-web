cf5be1cdbe5054f0122f3a10d4adc292ec10b7c0
Hermann <hermann.hausherr@gmail.com>
2024-07-18 22:54:27 +0200

    Added Italian language with some first translations

---
+++ app/lang/it/tb.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'added_new_plant' => 'I added a new plant: <a href="{url}">{name}</a>',
+    'moved_plant_to_history' => 'I moved <a href="{url}">{name}</a> to {history}',
+    'restored_plant_from_history' => 'I restored <a href="{url}">{name}</a> from {history}',
+    'deleted_plant' => 'I deleted <strong>{name}</strong>',
+    'created_task' => 'I created a new task: <a href="{url}">{name}</a>',
+    'completed_task' => 'I completed a task: <a href="{url}">{name}</a>',
+    'reactivated_task' => 'I reactivated a task: <a href="{url}">{name}</a>',
+    'created_inventory_item' => 'I created a new inventory item: <a href="{url}">{name}</a>',
+    'removed_inventory_item' => 'I removed item <strong>{name}</strong> from the inventory',
+    'added_calendar_item' => 'I added a new entry to the calendar: <a href="{url}">{name}</a>',
+    'edited_calendar_item' => 'I edited a calendar entry: <a href="{url}">{name}</a>'
+];
