750931000a0e1095d6aedf676f53239170f45ba8
Maciej Bryłka <maciejbrylka94@gmail.com>
2024-07-25 15:28:49 +0200

    change to impersonal form

--- app/lang/pl/tb.php
+++ app/lang/pl/tb.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'added_new_plant' => 'Dodałem nową roślinę: <a href="{url}">{name}</a>',
-    'moved_plant_to_history' => 'Przeniosłem roślinę <a href="{url}">{name}</a> do {history}',
-    'restored_plant_from_history' => 'Przywróciłem <a href="{url}">{name}</a> z {history}',
-    'deleted_plant' => 'Usunąłem <strong>{name}</strong>',
-    'created_task' => 'Stworzyłem nowe zadanie: <a href="{url}">{name}</a>',
-    'completed_task' => 'Ukończyłem zadanie: <a href="{url}">{name}</a>',
-    'reactivated_task' => 'Dezaktywowałem zadanie: <a href="{url}">{name}</a>',
-    'created_inventory_item' => 'Stworzyłem nowy przedmiot wyposażenia: <a href="{url}">{name}</a>',
-    'removed_inventory_item' => 'Usunąłem <strong>{name}</strong> z wyposażenia',
-    'added_calendar_item' => 'Dodałem nowy wpis w kalendarzu: <a href="{url}">{name}</a>',
-    'edited_calendar_item' => 'Edytowałem wpis z kalendarza: <a href="{url}">{name}</a>'
+    'added_new_plant' => 'Dodano nową roślinę: <a href="{url}">{name}</a>',
+    'moved_plant_to_history' => 'Przeniesiono roślinę <a href="{url}">{name}</a> do {history}',
+    'restored_plant_from_history' => 'Przywrócono <a href="{url}">{name}</a> z {history}',
+    'deleted_plant' => 'Usunięto <strong>{name}</strong>',
+    'created_task' => 'Stworzono nowe zadanie: <a href="{url}">{name}</a>',
+    'completed_task' => 'Ukończono zadanie: <a href="{url}">{name}</a>',
+    'reactivated_task' => 'Dezaktywowano zadanie: <a href="{url}">{name}</a>',
+    'created_inventory_item' => 'Stworzono nowy przedmiot wyposażenia: <a href="{url}">{name}</a>',
+    'removed_inventory_item' => 'Usunięto <strong>{name}</strong> z wyposażenia',
+    'added_calendar_item' => 'Dodano nowy wpis w kalendarzu: <a href="{url}">{name}</a>',
+    'edited_calendar_item' => 'Edytowano wpis z kalendarza: <a href="{url}">{name}</a>'
 ];
