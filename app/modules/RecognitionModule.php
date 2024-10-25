6477c016ca51220384c7b5d608d05819c2b74b92
Daniel Brendel <dbrendel1988@gmail.com>
2024-10-25 23:50:32 +0200

    #284 Identify via POST and GET requests

--- app/modules/RecognitionModule.php
+++ app/modules/RecognitionModule.php
@@ -9,18 +9,35 @@ class RecognitionModule {
     const API_ENDPOINT = 'https://my-api.plantnet.org/v2/identify/all';
 
     /**
-     * @param $file
-     * @param $organs
+     * @param $asset
      * @return mixed
      * @throws \Exception
      */
-    public static function identify($file, $organs = 'all')
+    public static function identify($asset)
     {
         try {
             if (!app('plantrec_enable')) {
                 throw new \Exception('Recognition feature is currently deactivated');
             }
 
+            if (strpos($asset, 'https://') !== false) {
+                return static::queryGet($asset);
+            } else {
+                return static::queryPost($asset);
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @param $asset
+     * @return mixed
+     * @throws \Exception
+     */
+    private static function queryPost($asset)
+    {
+        try {
             $ch = curl_init(self::API_ENDPOINT . '?api-key=' . app('plantrec_apikey'));
 
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -31,8 +48,36 @@ class RecognitionModule {
             ]);
 
             curl_setopt($ch, CURLOPT_POSTFIELDS, [
-                'images' => curl_file_create($file)
+                'images' => curl_file_create($asset)
             ]);
+
+            $response = curl_exec($ch);
+
+            $error = curl_error($ch);
+            if ((is_string($error)) && (strlen($error) > 0)) {
+                throw new \Exception($error);
+            }
+
+            curl_close($ch);
+
+            return json_decode($response);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @param $asset
+     * @return mixed
+     * @throws \Exception
+     */
+    private static function queryGet($asset)
+    {
+        try {
+            $ch = curl_init(self::API_ENDPOINT . '?api-key=' . app('plantrec_apikey') . '&images=' . $asset);
+
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, false);
 
             $response = curl_exec($ch);
 
