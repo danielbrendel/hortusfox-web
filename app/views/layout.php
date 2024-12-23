530dbd1e278e13dcec47c17e2114a4f5760e7038
Daniel Brendel <dbrendel1988@gmail.com>
2024-12-23 15:30:52 +0100

    #320 Use FontAwesome 6

--- app/views/layout.php
+++ app/views/layout.php
@@ -20,7 +20,7 @@
 		@else
 		<script src="{{ asset('js/vue.min.js') }}"></script>
 		@endif
-		<script src="{{ asset('js/fontawesome.js') }}"></script>
+		<script src="https://kit.fontawesome.com/c46e231beb.js" crossorigin="anonymous"></script>
 
 		@if (is_string(app('custom_head_code')))
 		{!! app('custom_head_code') !!}
