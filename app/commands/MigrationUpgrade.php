72ed3d329595144bfa3d450b69e53d4b810260b0
Daniel Brendel <dbrendel1988@gmail.com>
2025-02-23 15:23:41 +0100

    Resolves #353

--- app/commands/MigrationUpgrade.php
+++ app/commands/MigrationUpgrade.php
@@ -328,29 +328,48 @@ class MigrationUpgrade implements Asatru\Commands\Command {
     {
         try {
             $version = config('version');
-
-            if (count($args) > 0) {
-                $version = $args->get(0)->getValue();
+            
+            $history = [];
+            if (file_exists(app_path() . '/migrations/verhist.json')) {
+                $history = json_decode(file_get_contents(app_path() . '/migrations/verhist.json'));
             }
-
+            
             if (strpos($version, '.') === false) {
                 $version .= '.0';
             }
 
+            $prev_ver = (count($history) > 0) ? $history[count($history) - 1] : $version;
+            
             echo "Upgrading to \033[94m{$version}\033[39m...\n";
 
-            $method = 'upgradeTo' . str_replace('.', 'dot', $version);
+            list($startMajor, $startMinor) = explode('.', $prev_ver);
+            list($endMajor, $endMinor) = explode('.', $version);
 
-            if (method_exists($this, $method)) {
-                if (version_compare($version, '4.0', '>')) {
-                    $this->ensureTableMigration();
+            for ($major = $startMajor; $major <= $endMajor; $major++) {
+                $minStart = ($major == $startMajor) ? $startMinor : 0;
+                $minEnd = ($major == $endMajor) ? $endMinor : 9;
+
+                for ($minor = $minStart; $minor <= $minEnd; $minor++) {
+                    $method = 'upgradeTo' . $major . 'dot' . $minor;
+                    
+                    if (method_exists($this, $method)) {
+                        if (version_compare($version, '4.0', '>')) {
+                            $this->ensureTableMigration();
+                        }
+
+                        $this->$method();
+
+                        if (!in_array($version, $history)) {
+                            $history[] = $version;
+                        }
+
+                        file_put_contents(app_path() . '/migrations/verhist.json', json_encode($history));
+
+                        echo "\033[39m[\033[93m$major.$minor\033[39m] \033[32mDone!\033[39m\n";
+                    } else {
+                        echo "\033[93m[$major.$minor] Nothing to migrate or upgrade.\033[39m\n";
+                    }
                 }
-
-                $this->$method();
-
-                echo "\033[32mDone!\033[39m\n";
-            } else {
-                echo "\033[93mNothing to migrate or upgrade.\033[39m\n";
             }
         } catch (\Exception $e) {
             echo "\033[31mOperation failed: {$e->getMessage()}\033[39m\n";
