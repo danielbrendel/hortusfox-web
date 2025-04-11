df4a2f63d0579f0f8b86b25f4d5d8d531f98a4ff
Diego Gomes <diegomjua@gmail.com>
2025-04-11 19:26:03 -0300

    pt-BR Translation

---
+++ app/lang/pt-br/tb.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'added_new_plant' => 'Adicionei uma nova planta: <a href="{url}">{name}</a>',
+    'moved_plant_to_history' => 'Movi <a href="{url}">{name}</a> para {history}',
+    'restored_plant_from_history' => 'Restaurei <a href="{url}">{name}</a> de {history}',
+    'deleted_plant' => 'Deletei <strong>{name}</strong>',
+    'created_task' => 'Criei uma nova tarefa: <a href="{url}">{name}</a>',
+    'completed_task' => 'Comprei uma tarefa: <a href="{url}">{name}</a>',
+    'reactivated_task' => 'Reativei uma tarefa: <a href="{url}">{name}</a>',
+    'created_inventory_item' => 'Eu criei um novo item de inventário: <a href="{url}">{name}</a>',
+    'removed_inventory_item' => 'Removi um item <strong>{name}</strong> do inventário',
+    'added_calendar_item' => 'Adicionei uma nova entrada no calendário: <a href="{url}">{name}</a>',
+    'edited_calendar_item' => 'Editei uma entrada de calendário: <a href="{url}">{name}</a>'
+];
