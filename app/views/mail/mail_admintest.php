0f74efd27c7f0e10024f5ffe1c2f4a9dbc1b7ec6
Daniel Brendel <dbrendel1988@gmail.com>
2025-07-15 14:55:05 +0200

    Refine

--- app/views/mail/mail_admintest.php
+++ app/views/mail/mail_admintest.php
@@ -1,7 +1,6 @@
-<h1>{{ app('workspace') }}</h1>
+<h1>{{ __('app.hortusfox_version', ['version' => config('version')]) }}</h1>
 
 <p>
-    {{ __('app.hortusfox_version', ['version' => config('version')]) }}<br/>
     {{ __('app.php_version', ['version' => phpversion()]) }}<br/>
     {{ __('app.mysql_version', ['version' => VersionModel::getSqlVersion()]) }}<br/>
     {{ __('app.server_system_info', ['osn' => php_uname('s'), 'osv' => php_uname('v'), 'mt' => php_uname('m')]) }}<br/>
