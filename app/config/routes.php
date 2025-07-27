741337de2574d45e66a285bee708ad5f4c0b7e4a
Daniel Brendel <dbrendel1988@gmail.com>
2025-07-27 12:02:45 +0200

    Update route comment info

--- app/config/routes.php
+++ app/config/routes.php
@@ -6,10 +6,10 @@
     Add here all your needed routes.
 
     Schema:
-        [<url>, <method>, controller_file@controller_method]
+        [<url>, <method>, controller_file@controller_method, opt:unique_identifier]
     Example:
         [/my/route, get, mycontroller@index]
-        [/my/route/with/{param1}/and/{param2}, get, mycontroller@another]
+        [/my/route/with/{param1}/and/{param2}, get, mycontroller@another, mycontroller.another]
     Explanation:
         Will call index() in app\controller\mycontroller.php if request is 'get'
         Every route with $ prefix is a special route
