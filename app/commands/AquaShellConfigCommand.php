bec05a155487b3b3c90b1d4c981ee45ff2e4b9e6
Daniel Brendel <dbrendel1988@gmail.com>
2025-12-23 15:52:37 +0100

    Script improvements

--- app/commands/AquaShellConfigCommand.php
+++ app/commands/AquaShellConfigCommand.php
@@ -19,6 +19,10 @@ class AquaShellConfigCommand implements Asatru\Commands\Command {
         $url_config = '# AquaShell URL config, auto-generated at ' . date('Y-m-d H:i:s') . PHP_EOL . PHP_EOL;
         $url_config .= 'const APP_SERVICE_URL string <= "' . env('APP_SERVICE_URL') . '";' . PHP_EOL;
         $url_config .= 'const APP_GITHUB_URL string <= "' . env('APP_GITHUB_URL') . '";' . PHP_EOL;
+        $url_config .= 'const APP_GITHUB_SPONSOR string <= "' . env('APP_GITHUB_SPONSOR') . '";' . PHP_EOL;
+        $url_config .= 'const APP_DONATION_KOFI string <= "' . env('APP_DONATION_KOFI') . '";' . PHP_EOL;
+        $url_config .= 'const APP_SOCIAL_DISCORD string <= "' . env('APP_SOCIAL_DISCORD') . '";' . PHP_EOL;
+        $url_config .= 'const APP_SOCIAL_BLUESKY string <= "' . env('APP_SOCIAL_BLUESKY') . '";' . PHP_EOL;
         file_put_contents(base_path() . '/scripts/tmp/urls.conf.dnys', $url_config);
 
         $url_config = '# AquaShell database config, auto-generated at ' . date('Y-m-d H:i:s') . PHP_EOL . PHP_EOL;
