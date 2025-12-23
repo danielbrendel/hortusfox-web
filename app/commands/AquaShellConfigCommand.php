3ad94d53d386a939b688dd5bfcb4698f0c40cb66
Daniel Brendel <dbrendel1988@gmail.com>
2025-12-23 10:50:10 +0100

    #484 Implement command

---
+++ app/commands/AquaShellConfigCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+    Asatru PHP - Command handler
+*/
+
+/**
+ * Command handler class
+ */
+class AquaShellConfigCommand implements Asatru\Commands\Command {
+    /**
+     * Command handler method
+     * 
+     * @param $args
+     * @return void
+     */
+    public function handle($args)
+    {
+        $url_config = '# AquaShell URL config, auto-generated at ' . date('Y-m-d H:i:s') . PHP_EOL . PHP_EOL;
+        $url_config .= 'const APP_SERVICE_URL string <= "' . env('APP_SERVICE_URL') . '";' . PHP_EOL;
+        $url_config .= 'const APP_GITHUB_URL string <= "' . env('APP_GITHUB_URL') . '";' . PHP_EOL;
+        file_put_contents(base_path() . '/scripts/tmp/urls.conf.dnys', $url_config);
+
+        $url_config = '# AquaShell database config, auto-generated at ' . date('Y-m-d H:i:s') . PHP_EOL . PHP_EOL;
+        $url_config .= 'const DATABASE_HOST string <= "' . env('DB_HOST') . '";' . PHP_EOL;
+        $url_config .= 'const DATABASE_PORT int <= ' . env('DB_PORT') . ';' . PHP_EOL;
+        $url_config .= 'const DATABASE_NAME string <= "' . env('DB_DATABASE') . '";' . PHP_EOL;
+        $url_config .= 'const DATABASE_USER string <= "' . env('DB_USER') . '";' . PHP_EOL;
+        $url_config .= 'const DATABASE_PASS string <= "' . env('DB_PASSWORD') . '";' . PHP_EOL;
+        file_put_contents(base_path() . '/scripts/tmp/db.conf.dnys', $url_config);
+
+        echo "Successfully created AquaShell configuration files.\n";
+    }
+}
+    
--- app/config/commands.php
+++ app/config/commands.php
@@ -22,5 +22,6 @@ return [
     ['cache:clear', 'Clear the entire cache', 'CacheClearCommand'],
     ['theme:list', 'List all installed themes', 'ThemeListCommand'],
     ['theme:install', 'Install a theme', 'ThemeInstallCommand'],
-    ['theme:remove', 'Remove an installed theme', 'ThemeRemoveCommand']
+    ['theme:remove', 'Remove an installed theme', 'ThemeRemoveCommand'],
+    ['aquashell:config', 'Create config file for AquaShell scripts', 'AquaShellConfigCommand']
 ];
